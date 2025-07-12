c766db477484e547b3c0a3a1407f3f63a0357811
Alison Rocha <alison.sdr22@gmail.com>
2025-07-11 23:25:01 -0300

    Revert "feat: config postgres and rabbitmq in docker-compose"

--- src/PraestoPay.Infrastructure/Configuration/InfrastructureDependencyInjection.cs
+++ src/PraestoPay.Infrastructure/Configuration/InfrastructureDependencyInjection.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace PraestoPay.Infrastructure.Configuration;
@@ -8,11 +7,6 @@ public static class InfrastructureDependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<PraestoPayContext>(options =>
-        {
-            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"));
-        });
-
         return services;
     }
 }
